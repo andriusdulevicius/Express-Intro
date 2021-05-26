e298e21bfa181f56c573574081b3c6e58e3e6552
AndriusDulevicius <andriusdulevicius@email.com>
2021-05-26 13:25:17 +0300

    index.html added, sent file in response

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-'use strict';
+//isitraukiam express is npm
+const express = require('express');
+const path = require('path');
+//sukuriam express app objekta
+const app = express();
 
-console.log('express');
+const indexPath = path.join(__dirname, 'html', 'index.html');
+//resolve rasome kai viena paduodame __dirname , o join naudojame kai reikia paduoti daugiau kelia nurodanciu elementu
+
+// console.log(indexPath);
+
+//main route
+app.get('/', (req, res) => {
+  res.sendFile(indexPath);
+});
+
+//paleidziam serveri , kuris veikia localserver:3000 adresu, ir klausosi http ir kitu requestu nurodytu portu
+app.listen(3000, () => {
+  console.log('server is running');
+});
