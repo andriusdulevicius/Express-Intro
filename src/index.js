8468ec0ca5c48f4c5be25d344ca0581978601be8
AndriusDulevicius <andriusdulevicius@email.com>
2021-05-26 13:33:27 +0300

    added about.html , added links to pages

--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@ const path = require('path');
 const app = express();
 
 const indexPath = path.join(__dirname, 'html', 'index.html');
+const aboutPath = path.join(__dirname, 'html', 'about.html');
 //resolve rasome kai viena paduodame __dirname , o join naudojame kai reikia paduoti daugiau kelia nurodanciu elementu
 
 // console.log(indexPath);
 
 //main route
-app.get('/', (req, res) => {
-  res.sendFile(indexPath);
-});
+app.get('/', (req, res) => res.sendFile(indexPath));
+app.get('/about', (req, res) => res.sendFile(aboutPath));
 
 //paleidziam serveri , kuris veikia localserver:3000 adresu, ir klausosi http ir kitu requestu nurodytu portu
 app.listen(3000, () => {
