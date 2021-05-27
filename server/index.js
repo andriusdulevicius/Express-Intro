069b7d6f7e62d65ae06d6d825a5589217d68da70
AndriusDulevicius <andriusdulevicius@email.com>
2021-05-27 09:02:56 +0300

    eslint added

--- server/index.js
+++ server/index.js
@@ -1,16 +1,33 @@
 //isitraukiam express is npm
 const express = require('express');
-const path = require('path');
+const path = require('path'); //tai integruotas node modulis , kuris skirtas gauti absoliutiems ar reliatyviems keliams (w3 schoolse yra visi metodai galimi: join , resolve, dirname, extname, parse ir kt.)
+
 //sukuriam express app objekta
 const app = express();
 
 const { people } = require('./js/people');
 
-console.log(people);
+//Middle ware
+//Tarpine grandis atliekanti papildomus veiksmus pries ar po serverio uzklausu
+//pvz pries nueinant i svetaine, patikrinti ar zmogus turi teise ieiti i ta puslapi
+//logger
+
+const logger = (req, res, next) => {
+  console.log('logger in action');
+  console.log(`${req.protocol}://${req.get('host')}${req.originalUrl} on ${new Date().toLocaleTimeString()}`);
+
+  next();
+};
+//naudoti logger funkcija kaip Middle ware
+
+app.use(logger);
 
 //current paths
+// eslint-disable-next-line no-undef
 const htmlPath = path.join(__dirname, '../client', 'html');
+// eslint-disable-next-line no-undef
 const indexPath = path.join(__dirname, '../client', 'html', 'index.html');
+// eslint-disable-next-line no-undef
 const aboutPath = path.join(__dirname, '../client', 'html', 'about.html');
 //resolve rasome kai viena paduodame __dirname , o join naudojame kai reikia paduoti daugiau kelia nurodanciu elementu
 
@@ -18,7 +35,7 @@ const aboutPath = path.join(__dirname, '../client', 'html', 'about.html');
 
 //Routes
 app.get('/', (req, res) => res.sendFile(indexPath));
-// app.get('/about', (req, res) => res.sendFile(aboutPath));
+app.get('/about', (req, res) => res.sendFile(aboutPath));
 
 //our API
 app.get('/api/people', (req, res) => {
